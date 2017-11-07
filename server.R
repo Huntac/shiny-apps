fe472c7aec1b3a0ebac88a2e69a6c4e60eb4e577
Huntac <ch360409@gmail.com>
2017-11-07 08:34:22 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,31 +1,5 @@
 library(shiny)
 
-arg.info <-  list(
-  list(choices = c("Yes" = T, "No" = F),
-       prompt = quote(paste("Remove Missing Values for analysis?")),
-       type = "radioButtons"),
-  list(choices = c("not" = 0, "some" = 1, "very" = 3),
-       prompt = quote(paste("how cool are you?")),
-       type = "checkboxGroupInput"))
-
-
-set_output_automatically <- function(input, arg.info){
-  if(input[1] > 3){
-    arg.info <- arg.info[[1]]
-  } else {
-    arg.info <- arg.info[[2]]
-  }
-  do.call(arg.info$type, args=list(inputId=paste0("in", input), label = arg.info$prompt,
-                                   choices = arg.info$choices))
-  
-}
-
-ui <- fluidPage(
-  uiOutput('abcd'),
-  textOutput('value'),
-  uiOutput('result')
-)
-
 server <- function(input, output){
   
   output$abcd <- renderUI({
